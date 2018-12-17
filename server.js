3276d8870c866f1bdd4f0b103265a9bddaf5b176
ByPhoenix <egemenander@gmail.com>
2018-12-18 00:04:01 +0300

    Update server.js

--- server.js
+++ server.js
@@ -84,7 +84,7 @@ socket.on("ready user", function(ready)
 socket.on("day res", function(dayRes){
      dayResList.push(dayRes);
 
-     if(dayResList.length >= game.alivePlayers - 1){
+     if(dayResList.length > game.alivePlayers - 1){
 
           let decision = game.day(dayResList);
           console.log("DAY VOTE ENDED: "+ decision); //changes game.players based on Vote
