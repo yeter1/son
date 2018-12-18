19ad28f6ce3c849fc811e7c80dfbc2bd668170fe
ByPhoenix <egemenander@gmail.com>
2018-12-18 18:28:11 +0300

    Update server.js

--- server.js
+++ server.js
@@ -91,25 +91,25 @@ socket.on("day res", function(dayRes){
           dayResList=[];
 
           
-          if(game.isGameOver()){
-           gameOver();
-      }
-          else{
+                if(game.isGameOver()){
+                 gameOver();
+                  }
+                 else{
             
                 io.sockets.emit("day summary", decision);
                 serverNight();
                  //client needs to figure out who died based on change in .alive booleans (in response to day summary request)
                
                 for(i =0; i<game.players.length; i++){
-		    if(game.players[i].name === decision && game.players[i].role == 2){
+		               if(game.players[i].name === decision && game.players[i].role == 2){
 
-               hunterKilled();
-              }
+                           hunterKilled();
+                            }
 			
 		
-                     }
+                    }
                    
-           }
+                    }
      }
 });
 
